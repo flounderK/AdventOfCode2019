1cdf1f15928f842e191675561d412fb8b7a64749
Clif Wolfe <clifwolfe1@gmail.com>
2019-12-13 17:09:13 -0500

    Day2 part 2

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -1,10 +1,14 @@
 
 
 class Emul(object):
-    def __init__(self, opcodes):
-        self.program = opcodes
+    def __init__(self, opcodes, noun=None, verb=None):
+        self.program = opcodes.copy()
         self.inst_indx = 0
+        if isinstance(noun, int) and isinstance(verb, int):
+            self.program[1] = noun
+            self.program[2] = verb
         self.run()
+        self.output = self.program[0]
 
     def add(self):
         self.inst_indx += 1
@@ -14,7 +18,7 @@ class Emul(object):
         self.inst_indx += 1
         c = self.program[self.program[self.inst_indx]] = a + b
         self.inst_indx += 1
-        print(f"{a} + {b} = {c}")
+        # print(f"{a} + {b} = {c}")
 
     def mul(self):
         self.inst_indx += 1
@@ -24,7 +28,7 @@ class Emul(object):
         self.inst_indx += 1
         c = self.program[self.program[self.inst_indx]] = a * b
         self.inst_indx += 1
-        print(f"{a} + {b} = {c}")
+        # print(f"{a} * {b} = {c}")
 
     def run(self):
         while self.program[self.inst_indx] != 99:
@@ -33,7 +37,7 @@ class Emul(object):
             elif self.program[self.inst_indx] == 2:
                 self.mul()
             else:
-                print("unrecognized opcode")
+                # print("unrecognized opcode")
                 break
         # print(",".join([str(i) for i in self.program]))
 
@@ -41,10 +45,14 @@ class Emul(object):
 with open("Day2In.txt", "r") as f:
     content = [int(i) for i in f.read().split(",")]
 
-altered_content = content.copy()
-altered_content[1] = 12
-altered_content[2] = 2
-emulator = Emul(altered_content)
+emulator = Emul(content, 1, 2)
 
-p1res = emulator.program[0]
+p1res = emulator.output
 print(f"Part 1: {p1res}")
+
+for a, b in [(a, b) for a in range(99) for b in range(99)]:
+    emulator = Emul(content, a, b)
+    if emulator.output == 19690720:
+        break
+p2res = (100 * a) + b
+print(f"Part 2: {p2res}")
