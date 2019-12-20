3a79e4c562c761e7f2a2dad26bedd482e8ffca7b
Clif Wolfe <clifwolfe1@gmail.com>
2019-12-20 18:03:42 -0500

    Day5 part 2

--- Day5/Day5.py
+++ Day5/Day5.py
@@ -2,15 +2,16 @@
 
 
 class Emul(object):
-    def __init__(self, opcodes, noun=None, verb=None):
+    def __init__(self, opcodes, noun=None, verb=None, inbuf=None):
         self.program = opcodes.copy()
         self.inst_indx = 0
         if isinstance(noun, int) and isinstance(verb, int):
             self.program[1] = noun
             self.program[2] = verb
+        self.am = self.bm = self.cm = self.de = 0
+        self.inbuf = inbuf
         self.run()
         self.output = self.program[0]
-        self.am = self.bm = self.cm = self.de = 0
 
     def parse_mode(self, op):
         """Parse opcode for parameter mode mask"""
@@ -42,7 +43,7 @@ class Emul(object):
 
     def inp(self):
         op, a = self.program[self.inst_indx:self.inst_indx + 2]
-        self.program[a] = int(input("SIG INPUT>"))
+        self.program[a] = self.inbuf
         self.inst_indx += 2
 
     def out(self):
@@ -50,6 +51,29 @@ class Emul(object):
         res = self.mode(a, self.am)
         print(f"OUTPUT: {res}")
         self.inst_indx += 2
+
+    def jmp_if(self):
+        op, a, b = self.program[self.inst_indx:self.inst_indx + 3]
+        a = self.mode(a, self.am)
+        b = self.mode(b, self.bm)
+        if (self.de == 5 and a) or (self.de == 6 and not a):
+            self.inst_indx = b
+        else:
+            self.inst_indx += 3
+
+    def lt(self):
+        op, a, b, c = self.program[self.inst_indx:self.inst_indx + 4]
+        a = self.mode(a, self.am)
+        b = self.mode(b, self.bm)
+        self.program[c] = 1 if a < b else 0
+        self.inst_indx += 4
+
+    def eq(self):
+        op, a, b, c = self.program[self.inst_indx:self.inst_indx + 4]
+        a = self.mode(a, self.am)
+        b = self.mode(b, self.bm)
+        self.program[c] = 1 if a == b else 0
+        self.inst_indx += 4
 
     def run(self):
         self.parse_mode(self.program[self.inst_indx])
@@ -63,6 +87,12 @@ class Emul(object):
                 self.inp()
             elif self.de == 4:
                 self.out()
+            elif self.de in [5, 6]:
+                self.jmp_if()
+            elif self.de == 7:
+                self.lt()
+            elif self.de == 8:
+                self.eq()
             else:
                 print("unrecognized opcode")
                 break
@@ -73,6 +103,8 @@ class Emul(object):
 with open("Day5In.txt", "r") as f:
     content = [int(i) for i in f.read().split(",")]
 
-emulator = Emul(content)
+emulator = Emul(content, inbuf=1)
 print("Part 1: ^^")
+emulator = Emul(content, inbuf=5)
+print("Part 2: ^^")
 
